2245eb7ec58aee55e9b1a14b1675fec0bda007c1
Nadine <n4dinevid4l@hotmail.com>
2024-01-28 15:01:31 +0100

    JS Comments

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,8 @@
+/**
+ * Updates the main image in the gallery with the specified image path.
+ * @param {string} imagePath - The path of the image to be displayed.
+ * @returns None
+ */
 function updateGallery(imagePath) {
     document.getElementById('mainImage').src = imagePath;
 }
